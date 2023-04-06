3f986384bf7245c85d23a6cea7c3de89e60730cf
ladin22 <ladin22@naver.com>
2023-04-06 17:41:58 +0900

    [Class2][0406] Complete

---
+++ class2/1620.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int m, n;
+    
+    vectro<string> poketmon;
+
+    cin >> m >> n;
+
+    for(int i=0;i<m;i++)
+    {
+        string tmp;
+        cin >> tmp;
+
+        poketmon.push_back(tmp);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        
+    }
+    
+}
--- class2/18111.cpp
+++ class2/18111.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
+using namespace std;
 
 int main()
 {
     int ground[500][500];
     int n, m, b;
 
-    vector<pair<int,int>> height;
+    int result_time = 0x7f7f7f7f, result_height = -1;
 
     cin >> n >> m >> b;
 
@@ -15,9 +15,38 @@ int main()
     {
         for(int j=0;j<m;j++)
         {
-            int tmp
-            cin >> tmp;
-            ground[i][j] = tmp;
+            cin >> ground[i][j];
         }
     }
+    
+    for(int height=0;height<=256;height++)
+    {
+        int build = 0, remove = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                int diff = height - ground[i][j];
+
+                if(diff > 0) build += diff;
+                else remove += (diff * -1);
+            }
+        }
+
+        if(build <= b + remove)
+        {
+            int time = (build * 1) + (remove * 2);
+
+            if(time <= result_time)
+            {
+                if(result_height < height)
+                {
+                    result_time = time;
+                    result_height = height;
+                }
+            }
+        }
+    }
+
+    cout << result_time << " " << result_height;
 }
